fdb09342dfa2824107c84d32992e5f902e23f2ea
AmelianceSkyMusic <38717657+AmelianceSkyMusic@users.noreply.github.com>
2019-08-01 10:44:39 +0300

    Update

--- CUSTOM/ASM custom [ITEM] Select and move to item in next track (save cursor position).lua
+++ CUSTOM/ASM custom [ITEM] Select and move to item in next track (save cursor position).lua
@@ -1,4 +1,19 @@
-script_title = 'ASM [custom ITEM] Select and move to item in previous track (save cursor position)'
+--[[
+ * ReaScript Name: ASM custom [ITEM] Select and move to item in previous track (save cursor position)
+ * Author: Rsay Uaie (Ameliance SkyMusic)
+ * Author URI: https://forum.cockos.com/member.php?u=123975
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2019-03-25)
+  + Initial release
+--]]
+
+script_title = 'ASM custom [ITEM] Select and move to item in previous track (save cursor position)'
 
 local info = debug.getinfo(1,'S')
 local script_path = info.source:match([[^@?(.*[\/])[^\/]-$]])
@@ -8,7 +23,7 @@ reaper.Undo_BeginBlock()
 reaper.PreventUIRefresh(1)
 
 asm.doCmdID('_BR_SAVE_CURSOR_POS_SLOT_1', 'MAIN') --SWS/BR: Save edit cursor position, slot 01
-asm.doCmdID('40418', 'MAIN') --Item navigation: Select and move to item in previous track
+asm.doCmdID('40419', 'MAIN') --Item navigation: Select and move to item in next track
 asm.doCmdID('_BR_RESTORE_CURSOR_POS_SLOT_1', 'MAIN') --Item navigation: Select and move to item in previous track
 
 reaper.PreventUIRefresh(-1)
